9931553fd9630b224909cb3f5fb23a58dcadf5b5
Dhyanesh Damania <d2githubfake@fake.com>
2023-08-26 11:05:28 +0530

    Add sync_pictures.sh and background task for Mac.

--- sync/sync_pictures.sh
+++ sync/sync_pictures.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-#
 
-rsync -auz /Volumes/photo/Pictures/*.txt /Users/dhyanesh/Pictures
+LOC1=$1
+LOC2=$2
+
+rsync -avuz $LOC1/ $LOC2 > /tmp/sync_pictures.error 2>&1
+rsync -avuz $LOC2/ $LOC1 > /tmp/sync_pictures.error 2>&1
